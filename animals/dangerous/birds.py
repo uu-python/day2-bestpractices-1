5fca6246728244924d84408154e5f688f10ca79d
Filipe Maia <filipe.c.maia@gmail.com>
2019-03-04 21:31:16 +0100

    Revert "Added solutions to exercises"
    This reverts commit 852256e5e0a4f1b527e8152e4bbbe180598ed144.

--- animals/dangerous/birds.py
+++
@@ -1,9 +0,0 @@
-class Birds:
-    def __init__(self):
-        ''' Constructor for this class. '''
-        # Create some member animals
-        self.members = []
- 
-    def printMembers(self):
-        print('Printing members of the dangerous Birds class')
-        print('There are no dangerous birds')
